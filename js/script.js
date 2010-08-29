401cffd50aad65a8b02a7ef584d982aa93e00a49
skilldrick <skilldrick@gmail.com>
2010-08-29 10:50:12 +0100

    Started drawing walls.

--- js/script.js
+++ js/script.js
@@ -10,17 +10,38 @@ $(document).ready(function () {
 var JsCave = JsCave || {};
 
 JsCave.Game = (function () {
-    var that = {};
+    var that = {},
+        canvas,
+        ctx,
+        width,
+        height;
 
     that.start = function () {
         canvas = $('#game-board')[0];
+        width = canvas.width;
+        height = canvas.height;
+        
         if(canvas.getContext) {
             ctx = canvas.getContext('2d');
             i = 1;
-            JsCave.ctx = ctx;
-            setInterval(draw, 250);
-            setInterval(JsCave.Walls.draw, 250);
+            JsCave.ctx = that.ctx = ctx;
+            that.gameLoop();
         }
+    }
+
+    that.gameLoop = function () {
+        that.draw();
+        setTimeout(that.gameLoop, 200);
+    }
+
+    that.draw = function () {
+        that.ctx.clearRect(0, 0, width, height);
+        JsCave.Walls.draw();
+        draw_border();
+    }
+
+    function draw_border() {
+        ctx.strokeRect(1, 1, width - 2, width -2);
     }
 
     return that;
@@ -29,9 +50,17 @@ JsCave.Game = (function () {
 
 JsCave.Walls = (function () {
     var that = {};
+    var width = 100;
+    var offset = 0;
+    var offArray = [50, 51, 52, 53, 54, 55, 54, 53, 52, 51];
+    var counter = 0;
+    var blockSize = 5;
+    
 
     that.draw = function () {
-        JsCave.ctx.strokeRect(10, 10, 20, 30);
+        for(var i = 0; i < offArray.length; i+=1) {
+            JsCave.ctx.fillRect(i * blockSize, 0, blockSize, offArray[i]);
+        }
     }
 
     return that;
